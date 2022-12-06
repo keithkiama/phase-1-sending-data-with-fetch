97ac5411758953563ab20c7502976b34f3e57aee
keithkiama <keithkiama2003@gmail.com>
2022-12-06 10:57:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const url = ('http://localhost:3000/users')
+
+function submitData(name, email) {
+  return fetch(url,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+
+  .then(response => { return response.json()})
+
+  .then((object) => {document.body.innerHTML = object["id"]})
+
+  .catch(error =>{document.body.innerHTML = error.message;})
+}
+
